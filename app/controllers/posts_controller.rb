f5d7ae2328545eb266661762e560fa0d41df1f92
Omar Ismael <omar0star@yahoo.com>
2020-04-03 12:10:59 +0300

    created develop branch

--- app/controllers/posts_controller.rb
+++ app/controllers/posts_controller.rb
@@ -27,6 +27,7 @@ class PostsController < ApplicationController
     @post = Post.new(post_params)
     @post.user_id = current_user.id
     
+    
     respond_to do |format|
       if @post.save
         format.html { redirect_to @post, notice: 'Post was successfully created.' }
