85af2aceb153c1e8d7f76a3b268164981c987c8a
Omar Ismael <omar0star@yahoo.com>
2020-04-06 13:08:00 +0300

    added active storage

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -25,6 +25,7 @@ class UsersController < ApplicationController
     @user = User.create(params.require(:user).permit(:username,        
       :password))
     session[:user_id] = @user.id
+    @user.avatar.attach(params[:avatar])
     redirect_to '/posts'
   end
 
@@ -33,6 +34,7 @@ class UsersController < ApplicationController
   def update
     respond_to do |format|
       if @user.update(user_params)
+        @user.avatar.attach(params[:avatar])
         format.html { redirect_to @user, notice: 'User was successfully updated.' }
         format.json { render :show, status: :ok, location: @user }
       else
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, :url => "/:class/:attachment/:id/:style_:filename"
-    validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
+    has_one_attached :avatar
+    
 end
