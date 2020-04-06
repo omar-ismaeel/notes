24af6ee61aaafc1095a41e84e971cfb80a104727
Omar Ismael <omar0star@yahoo.com>
2020-04-06 12:19:13 +0300

    Fixed Profile pictures path

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,6 @@
 class User < ApplicationRecord
     has_secure_password
     has_many :posts
-    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, default_url: "/images/:style/missing.png"
+    has_attached_file :avatar, styles: { medium: "300x300>", thumb: "100x100>" }, :url => "/:class/:attachment/:id/:style_:filename"
     validates_attachment_content_type :avatar, content_type: /\Aimage\/.*\z/
 end
