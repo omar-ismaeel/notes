9de27b709a9f1ddda224c9317c033a763ea0cfd1
Omar Ismael <omar0star@yahoo.com>
2020-04-08 16:31:33 +0300

    Added Security

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -23,10 +23,19 @@ class UsersController < ApplicationController
   # POST /users.json
   def create
     @user = User.create(params.require(:user).permit(:username,        
-      :password))
+      :password_digest))
     session[:user_id] = @user.id
-    redirect_to '/posts'
+    
+    respond_to do |format|
+      if @user.save
+        format.html { redirect_to @user, notice: 'User was successfully created.' }
+        format.json { render :show, status: :created, location: @user }
+      else
+        format.html { render :new }
+        format.json { render json: @user.errors, status: :unprocessable_entity }
+      end
   end
+end
 
   # PATCH/PUT /users/1
   # PATCH/PUT /users/1.json
